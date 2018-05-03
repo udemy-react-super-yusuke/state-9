1b3786f2b266a6e18e989f6b93489f337c4f66f9
udemy-react-super-yusuke <38956215+udemy-react-super-yusuke@users.noreply.github.com>
2018-05-04 01:21:49 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const array1 = [0, 1, 2, 3, 4];
+
+const newArray1 = array1.map((output, index) => {
+  return `${index}番目は${output}です。`;
+});
+
+//console.log(newArray1);
+
+const array2 = ["tanaka", "nakanishi", "sasaki", "koyama", "oda"];
+
+const newArray2 = array2.map((output, index) => {
+  return `${index}番目は${output}さんです。`;
+});
+
+console.log(newArray2);
